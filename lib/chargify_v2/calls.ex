2e41cb930b0f2542601a6fe604268bfa1ce5d98d
Wendy Smoak <wsmoak@gmail.com>
2015-08-24 08:17:40 -0400

    Add docs for Calls errors and error_messages

--- lib/chargify_v2/calls.ex
+++ lib/chargify_v2/calls.ex
@@ -17,10 +17,16 @@ defmodule ChargifyV2.Calls do
     ChargifyV2.get!("/calls/#{call_id}")
   end
 
+  @doc """
+  Returns a list of maps, each map containing the keys attribute and message
+  """
   def errors(response) do
     response.body[:call]["response"]["result"]["errors"]
   end
 
+  @doc """
+  Returns a list of strings
+  """
   def error_messages(response) do
     response
     |> errors
